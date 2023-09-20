e24d39c03cb04acb8875c5bcfd124a689b13b10b
Papy Dia <papydia53@gmail.com>
2023-09-20 16:05:23 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const cours = {
+    titreCours : "",
+    professeur : "",
+    salle : parseInt(),
+};
+
+const professeurs = {
+    nom : "",
+    prenom : "",
+    email : "",
+    numeroTel : parseInt()
+};
+
+const etudiants = {
+    nom : "",
+    prenom : "",
+    numeroCarte : parseInt(),
+    numeroTel : parseInt()
+};
+
